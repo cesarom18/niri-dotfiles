9bad3a436d2f6a8778a966e7d7f15e56bf17c111
cesarom18 <cesarosses885@gmail.com>
2026-02-14 17:25:31 -0300

    Actualizacion dotfiles

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -6,7 +6,7 @@ return {
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 		local eslint = lint.linters.eslint_d
 
-		-- if Eslint error configuration not found : change MasonInstall eslint@version or npm i -g eslint at a specific version
+		-- Linters by filetype
 		lint.linters_by_ft = {
 			javascript = {"biomejs"},
 			typescript = {"biomejs"},
@@ -16,6 +16,15 @@ return {
 			python = { "pylint" },
 		}
 
+		lint.linters.pylint.cmd = "python"
+		lint.linters.pylint.args = {
+			"-m",
+			"pylint",
+			"-f",
+			"json",
+		}
+
+		-- Config eslint_d
 		eslint.args = {
 			"--no-warn-ignored",
 			"--format",
@@ -23,10 +32,11 @@ return {
 			"--stdin",
 			"--stdin-filename",
 			function()
-                return vim.fn.expand("%:p")
+				return vim.fn.expand("%:p")
 			end,
 		}
 
+		-- Autocmd for lint
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			group = lint_augroup,
 			callback = function()
@@ -34,6 +44,7 @@ return {
 			end,
 		})
 
+		-- Keymap
 		vim.keymap.set("n", "<leader>l", function()
 			lint.try_lint()
 		end, { desc = "Trigger linting for current file" })
