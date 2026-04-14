00de17f8fd0ef8da08fde207380246e8148b8a59
cesarom18 <cesarosses885@gmail.com>
2026-04-14 17:06:10 -0400

    Actualizacion dotfiles

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -4,7 +4,7 @@ return {
 	dependencies = {
 		"williamboman/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		"saghen/blink.cmp",
+		"hrsh7th/cmp-nvim-lsp",
 		"neovim/nvim-lspconfig",
 	},
 
