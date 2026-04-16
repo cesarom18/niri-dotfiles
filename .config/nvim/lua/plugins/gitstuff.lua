0be7af4816f0c1a766b3ed557620d805b5e57279
cesarom18 <cesarosses885@gmail.com>
2026-04-16 11:56:21 -0400

    Actualizacion dotfiles

--- .config/nvim/lua/plugins/gitstuff.lua
+++ .config/nvim/lua/plugins/gitstuff.lua
@@ -1,39 +1,77 @@
 return {
-    -- For all git plugins 
+	-- For all git plugins
 	{
 		"lewis6991/gitsigns.nvim",
 		event = { "BufReadPre", "BufNewFile" },
 		opts = {
-			on_attach = function(bufnr)
-				local gs = package.loaded.gitsigns
-				local function map(mode, l, r, desc)
-					vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
-				end
-				-- Navigation
-				map("n", "]h", gs.next_hunk, "Next Hunk")
-				map("n", "[h", gs.prev_hunk, "Prev Hunk")
-			end,
+			signs = {
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+			signs_staged = {
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+			signs_staged_enable = true,
+			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+			numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+			linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+			word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+			watch_gitdir = {
+				follow_files = true,
+			},
+			auto_attach = true,
+			attach_to_untracked = false,
+			current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+				delay = 1000,
+				ignore_whitespace = false,
+				virt_text_priority = 100,
+				use_focus = true,
+			},
+			current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
+			blame_formatter = nil, -- Use default
+			sign_priority = 6,
+			update_debounce = 100,
+			status_formatter = nil, -- Use default
+			max_file_length = 40000, -- Disable if file is longer than this (in lines)
+			preview_config = {
+				-- Options passed to nvim_open_win
+				style = "minimal",
+				relative = "cursor",
+				row = 0,
+				col = 1,
+			},
 		},
 	},
-    -- Lazy git 
-    {
-        "kdheepak/lazygit.nvim",
-        --NOTE: Trying out lazygit in Snacks nvim
-        enabled = true,
-        cmd = {
-            "LazyGit",
-            "LazyGitConfig",
-            "LazyGitCurrentFile",
-            "LazyGitFilter",
-            "LazyGitFilterCurrentFile",
-        },
-        -- Window border thing
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-        -- Setting up with keys={} allows plugin to load when command runs at the start
-        keys = {
-            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
-        },
-    }
+	-- Lazy git
+	{
+		"kdheepak/lazygit.nvim",
+		enabled = true,
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- Window border thing
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		-- Setting up with keys={} allows plugin to load when command runs at the start
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+		},
+	},
 }
